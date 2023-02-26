2f4ffcf57c56f773b42f6887ea68e706ec69b1f4
Achmad Nugraha <95465555+Achmadnugraha11@users.noreply.github.com>
2023-02-26 12:53:28 +0700

    Fixed

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ const targetObject = document.getElementById('targetObject')
 const inputHexa = document.getElementById('inputHexa')
 
 function ubahLatar() {
- //inputHexa.style.color = input.value
- targetObject.style.backGround = targetObject.value
+ 
+ targetObject.style.backgroundColor = inputHexa.value
 }
